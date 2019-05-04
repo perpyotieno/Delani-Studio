4b5cb282c402d23bef11520f7aa8ee9c3393b5df
Perpetua Mary <perpetuamary@yahoo.com>
2019-05-04 15:31:28 +0300

    add a jquery function with a toggle,show and hide method to make the images and text toggle evry time you click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,50 @@
-var output= function(name){
-  return name;
-}
+// var output= function(name){
+//   return name;
+// }
+//
+// $(document).ready(function(){
+//   $(#fill form).submit(function(event){
+//     event.preventDefault();
+//     var name =$("input#name").val();
+//     var email= $("input#email").val();
+//     var message=$("input#message").val();
+//
+//     alert(output + "we have received your message. Thank you for reaching out to us.")
+//   })
+// })
 
 $(document).ready(function(){
-  $(#fill form).submit(function(event){
-    event.preventDefault();
-    var name =$("input#name").val();
-    var email= $("input#email").val();
-    var message=$("input#message").val();
+  $(".design").click(function(){
+    $(".design").hide();
+    $(".designp").toggle();
+    $(".designp").show();
+  })
+  $(".designp").click(function(){
+    $(".design").toggle();
+    $(".designp").hide();
+    $(".design").show();
+  })
 
-    alert(output + "we have received your message. Thank you for reaching out to us.")
+  $(".development").click(function(){
+    $(".development").hide();
+    $(".developmentp").toggle();
+    $(".developmentp").show();
+  })
+  $(".developmentp").click(function(){
+    $(".development").toggle();
+    $(".developmentp").hide();
+    $(".development").show();
+  })
+
+  $(".product-management").click(function(){
+    $(".product-management").hide();
+    $(".product-managementp").toggle();
+    $(".product-managementp").show();
+
+  })
+  $(".product-managementp").click(function(){
+    $(".product-management").toggle();
+    $(".product-managementp").hide();
+    $(".product-management").show();
   })
 })
